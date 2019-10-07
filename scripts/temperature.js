464b28b2932bf0aafd7d2aeacb36de33d07b298d
Joseph Watts <josephcw@live.com>
2019-10-07 18:53:49 -0500

    Complete basic conversion calculator

--- scripts/temperature.js
+++ scripts/temperature.js
@@ -22,4 +22,43 @@ function convertFromFarenheitToCelsius(temperatureInFarenheit) {
     return (temperatureInFarenheit - 32) * (5/9)
 }
 
+document.getElementById("submit-button").addEventListener("click", ()=> {
+    // Find out which radio button option was selected to convert from.
+    let tempCelsius = 0
+    let tempFarenheit = 0
+    let tempKelvin = 0
 
+    // Select radio buttons
+    let radios = document.getElementsByName("fromRadio")
+    checked = 0
+    for (let i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            checked = i
+        }
+    }
+    //console.log(checked)
+    // 0 is temp celsius
+    // 1 is farenheit
+    // 2 is kelvin
+
+    if (checked == 0) {
+        tempCelsius = parseFloat(document.getElementById("temp-celsius-input").value)
+        tempFarenheit = convertFromCelsiusToFarenheit(tempCelsius)
+        tempKelvin = convertFromCelsiusToKelvin(tempCelsius)
+    } else if (checked == 1) {
+        tempFarenheit = parseFloat(document.getElementById("temp-farenheit-input").value)
+        tempCelsius = convertFromFarenheitToCelsius(tempFarenheit)
+        tempKelvin = convertFromFarenheitToKelvin(tempFarenheit)
+    } else {
+        tempKelvin = parseFloat(document.getElementById("temp-kelvin-input").value)
+        tempCelsius = convertFromKelvinToCelsius(tempKelvin)
+        tempFarenheit = convertFromKelvinToFarenheit(tempKelvin)
+    }
+    // console.log(tempCelsius)
+    // console.log(tempFarenheit)
+    // console.log(tempKelvin)
+    // Update the text fields
+    document.getElementById("tempC").innerHTML = tempCelsius.toFixed(2)
+    document.getElementById("tempF").innerHTML = tempFarenheit.toFixed(2)
+    document.getElementById("tempK").innerHTML = tempKelvin.toFixed(2)
+})
