468ec3e654bf777d02387c4be5ff402f5a8aec2a
Joseph Watts <josephcw@live.com>
2019-10-07 18:17:31 -0500

    Save progress

--- scripts/temperature.js
+++ scripts/temperature.js
@@ -1,19 +1,25 @@
-function convertFromCelsiusToKelvin() {
-
+function convertFromCelsiusToKelvin(temperatureInCelsius) {
+    return temperatureInCelsius + 273.15
 }
 
-function convertFromKelvinToCelsius() {
-
+function convertFromKelvinToCelsius(temperatureInKelvin) {
+    return temperatureInKelvin - 273.15
 }
 
-function convertFromFarenheitToKelvin() {
-
+function convertFromFarenheitToKelvin(temperatureInFarenheit) {
+    return convertFromCelsiusToKelvin(convertFromFarenheitToCelsius(temperatureInFarenheit))
 }
 
-function convertFromCelsiusToFarenheit() {
-
+function convertFromCelsiusToFarenheit(temperatureInCelsius) {
+    return ((9/5) * temperatureInCelsius) + 32
 }
 
-function convertFromKelvinToFarenheit() {
-
+function convertFromKelvinToFarenheit(temperatureInKelvin) {
+    return convertFromCelsiusToFarenheit(convertFromKelvinToCelsius(temperatureInKelvin))
 }
+
+function convertFromFarenheitToCelsius(temperatureInFarenheit) {
+    return (temperatureInFarenheit - 32) * (5/9)
+}
+
+
--- scripts/test/tests.js
+++ scripts/test/tests.js
@@ -1,3 +1,8 @@
-QUnit.test( "hello test", function( assert ) {
-    assert.ok( 1 == "1", "Passed!" );
+QUnit.test( "Test Temperature Conversions", function(assert) {
+    assert.equal(273.15, convertFromCelsiusToKelvin(0));
+    assert.equal(-40, convertFromCelsiusToFarenheit(-40));
+    assert.equal(-40, convertFromFarenheitToCelsius(-40));
+    assert.equal(-273.15, convertFromKelvinToCelsius(0));
+    assert.equal(32, convertFromCelsiusToFarenheit(0));
+    assert.equal(0, convertFromFarenheitToCelsius(32));
 });
